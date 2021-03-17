37fe2e9b59e098ca639ba075a96663c328c25cb6
udeshar <46628117+udeshar@users.noreply.github.com>
2021-03-17 21:08:32 +0530

    Add files via upload

---
+++ Final Sparkplus website/js/numberOnLines.js
@@ -0,0 +1,28 @@
+function changeSlideNumber(){
+
+        if(slideNumber==1){
+            setTimeout(() => {
+               document.getElementById("valueToChange").innerText="1"; 
+            }, 1000);
+            
+        }
+        if(slideNumber==2){
+            setTimeout(() => {
+               document.getElementById("valueToChange").innerText="2"; 
+            }, 1000);
+            
+        }
+        if(slideNumber==3){
+            setTimeout(() => {
+               document.getElementById("valueToChange").innerText="3"; 
+            }, 1000);
+            
+        }
+        if(slideNumber==4){
+            setTimeout(() => {
+               document.getElementById("valueToChange").innerText="4"; 
+            }, 1000);
+            
+        }
+    
+}
