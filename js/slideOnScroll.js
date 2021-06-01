df534d0a60cb87aa734e75d685c0c3cc7ce72c96
udeshar <udesh.arlekar@gmail.com>
2021-06-01 12:52:19 +0530

    added swipe event

--- js/slideOnScroll.js
+++ js/slideOnScroll.js
@@ -10,138 +10,10 @@ function slideOnScroll(evt) {
     evt.preventDefault();
 
     if(delta > 0){
-        //go up
-        if(slideNumber==1){
-            // $(".intro").addClass("slide-left-lscr")
-            // $(".dessert").css("display","block")
-            // setTimeout(() => {
-            //     $(".intro").css("zIndex","10");
-            //     $(".dessert").css("zIndex","11")
-            //     $(".intro").removeClass("slide-left-lscr");
-            //     $(".intro").css("display","none")
-            // }, 1000);
-            // slideNumber=4;
-            // changeSlideNumber()
-            // executeSideNav(slideNumber)
-            // return
-            showDessert();
-            return;
-        }
-        if(slideNumber==2){
-            // $(".forest").addClass("slide-left-lscr")
-            // $(".intro").css("display","block")
-            // setTimeout(() => {
-            //     $(".forest").css("zIndex","10");
-            //     $(".intro").css("zIndex","11")
-            //     $(".forest").removeClass("slide-left-lscr");
-            //     $(".forest").css("display","none")
-            // }, 1000);
-            // slideNumber--;
-            // changeSlideNumber()
-            // executeSideNav(slideNumber)
-            // return
-            showIntro();
-            return;
-        }
-        if(slideNumber==3){
-            // $(".ocean").addClass("slide-left-lscr")
-            // $(".forest").css("display","block")
-            // setTimeout(() => {
-            //     $(".ocean").css("zIndex","10");
-            //     $(".forest").css("zIndex","11")
-            //     $(".ocean").removeClass("slide-left-lscr");
-            //     $(".ocean").css("display","none")
-            // }, 1000);
-            // slideNumber--;
-            // changeSlideNumber()
-            // executeSideNav(slideNumber)
-            // return
-            showForest();
-            return;
-        }
-        if(slideNumber==4){
-            // $(".dessert").addClass("slide-left-lscr")
-            // $(".ocean").css("display","block")
-            // setTimeout(() => {
-            //     $(".dessert").css("zIndex","10");
-            //     $(".ocean").css("zIndex","11")
-            //     $(".dessert").removeClass("slide-left-lscr");
-            //     $(".dessert").css("display","none")
-            // }, 1000);
-            // slideNumber--;
-            // changeSlideNumber()
-            // executeSideNav(slideNumber)
-            // return
-            showOcean();
-            return;
-        }
+        slideGoUp();
     }
     else{
-        //go down
-        if(slideNumber==1){
-            // $(".intro").addClass("slide-left-lscr");
-            // $(".forest").css("display","block")
-            // setTimeout(() => {
-            //     $(".intro").css("zIndex","10")
-            //     $(".intro").removeClass("slide-left-lscr");
-            //     $(".intro").css("display","none")
-            //     $(".forest").css("zIndex","11")
-            // }, 1000);
-            // slideNumber++;
-            // changeSlideNumber()
-            // executeSideNav(slideNumber)
-            // return
-            showForest();
-            return;
-        }
-        if(slideNumber==2){
-            // $(".forest").addClass("slide-left-lscr");
-            // $(".ocean").css("display","block")
-            // setTimeout(() => {
-            //     $(".forest").css("zIndex","10")
-            //     $(".forest").removeClass("slide-left-lscr");
-            //     $(".forest").css("display","none")
-            //     $(".ocean").css("zIndex","11")
-            // }, 1000);
-            // slideNumber++;
-            // changeSlideNumber()
-            // executeSideNav(slideNumber)
-            // return
-            showOcean();
-            return;
-        }
-        if(slideNumber==3){
-            // $(".ocean").addClass("slide-left-lscr");
-            // $(".dessert").css("display","block")
-            // setTimeout(() => {
-            //     $(".ocean").css("zIndex","10")
-            //     $(".ocean").removeClass("slide-left-lscr");
-            //     $(".ocean").css("display","none")
-            //     $(".dessert").css("zIndex","11")
-            // }, 1000);
-            // slideNumber++;
-            // changeSlideNumber()
-            // executeSideNav(slideNumber)
-            // return
-            showDessert();
-            return;
-        }
-        if(slideNumber==4){
-            // $(".dessert").addClass("slide-left-lscr");
-            // $(".intro").css("display","block")
-            // setTimeout(() => {
-            //     $(".dessert").css("zIndex","10")
-            //     $(".dessert").removeClass("slide-left-lscr");
-            //     $(".dessert").css("display","none")
-            //     $(".intro").css("zIndex","11")
-            // }, 1000);
-            // slideNumber=1;
-            // changeSlideNumber()
-            // executeSideNav(slideNumber)
-            // return
-            showIntro();
-            return;
-        }
+        slideGoDown();
     }   
           
 }
@@ -397,10 +269,46 @@ function activeNav(){
    
 }
 
+function slideGoUp(){
+    //go up
+    if(slideNumber==1){
+        showDessert();
+        return;
+    }
+    if(slideNumber==2){
+        showIntro();
+        return;
+    }
+    if(slideNumber==3){
+        showForest();
+        return;
+    }
+    if(slideNumber==4){
+        showOcean();
+        return;
+    }
+}
+function slideGoDown(){
+    //go down
+    if(slideNumber==1){
+        showForest();
+        return;
+    }
+    if(slideNumber==2){
+        showOcean();
+        return;
+    }
+    if(slideNumber==3){
+        showDessert();
+        return;
+    }
+    if(slideNumber==4){
+        showIntro();
+        return;
+    }
+}
+
 // ****************************************** for mobile devices **************************************************
 
-$( ".home-container" ).on( "swiperight", swiperightHandler );
-
-function swiperightHandler(){
-    
-}
+$( ".home-container" ).on( "swiperight", slideGoUp );
+$( ".home-container" ).on( "swipeleft", slideGoDown );
